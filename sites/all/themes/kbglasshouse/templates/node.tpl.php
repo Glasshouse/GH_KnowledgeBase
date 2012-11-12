91236310ecb50ca45c9dd39d19ce132cf0aff3be
Paul BERNARD <paul@glasshouse.fr>
2012-11-12 09:30:05 +0100

    FIX Notice : undefined index on $body variable in node.tpl.php

--- sites/all/themes/kbglasshouse/templates/node.tpl.php
+++ sites/all/themes/kbglasshouse/templates/node.tpl.php
@@ -100,7 +100,7 @@
       // We hide the comments and links now so that we can render them later.
       hide($content['comments']);
       hide($content['links']);
-      print $body['value'];
+      if (isset($body['value'])) print $body['value'];
     ?>
   </div>
 
